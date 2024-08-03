e137bfb02917afc45a17d8ddde31836eb1ee9ad6
tree <hhyjs2r@gmail.com>
2024-08-03 17:39:05 +0900

    feat: creat login module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoginModule } from './login/login.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         synchronize: true,
       }),
     }),
+    LoginModule,
   ],
   controllers: [AppController],
   providers: [AppService],
